7dec626b00781da652e54070ad8cb6fcc59323a9
Givemegitpls <veryangrybarmaley@gmail.com>
2025-03-16 15:53:06 +0300

    Hyprland config was updated

---
+++ private_dot_config/hypr/scripts/executable_eDPower.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+check=$(hyprctl monitors | awk '/HDMI-A-1/{print $1}' | tr -d '\n')
+
+if [ $check != '' ]; then
+  hyprctl keyword monitor "eDP-1, disable"
+  swww kill
+  swww-daemon
+fi
