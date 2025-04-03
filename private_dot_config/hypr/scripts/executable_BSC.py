94d61e1d211fe0717ac5e60189b6ca5413155f1b
Givemegitpls <veryangrybarmaley@gmail.com>
2025-04-03 22:20:20 +0300

    Battery notifier fix

---
+++ private_dot_config/hypr/scripts/executable_BSC.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from psutil import sensors_battery, process_iter
+import os
+import time
+from setproctitle import setproctitle
+
+
+def main():
+    while True:
+        if sensors_battery().power_plugged:
+            os.system('dunstify -u normal -r "6896" "Battery" "Chargind"')
+            while sensors_battery().power_plugged:
+                time.sleep(2)
+            os.system('dunstify -u normal -r "6896" "Battery" "Discharging"')
+        elif sensors_battery().percent <= 15:
+            os.system('dunstify -u critical -r "6896" "Battery" "Low battery alarm"')
+            while not sensors_battery().power_plugged or sensors_battery().percent > 5:
+                time.sleep(2)
+        if sensors_battery().percent <= 5:
+            os.system("systemctl suspend")
+            time.sleep(60)
+
+
+app_name = "BSC"
+
+for proc in process_iter():
+    if proc.name() == app_name and proc.pid != os.getpid():
+        print(proc)
+        os.kill(proc.pid, 9)
+
+
+if __name__ == "__main__":
+    setproctitle(app_name)
+    list_of_procs = []
+    main()
