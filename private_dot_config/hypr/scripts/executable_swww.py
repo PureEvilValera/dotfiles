53d0e7b4c9e734ce0259494407d38d2010316765
Alexey Korneev <korneev@apsolutions.ru>
2025-04-08 13:00:47 +0300

    cp has been replaced with ln in swww script

--- private_dot_config/hypr/scripts/executable_swww.py
+++ private_dot_config/hypr/scripts/executable_swww.py
@@ -1,14 +1,14 @@
 import os
 from random import randrange
 
-swww_output = str(os.popen('swww query').read())
+swww_output = str(os.popen("swww query").read())
 swww_output = swww_output.split(" ")
-path = swww_output[len(swww_output) - 1].replace('\n', '')
+path = swww_output[len(swww_output) - 1].replace("\n", "")
 current = path.split("/")
 current = current[len(current) - 1]
 
-images = (os.listdir(path.replace(current, '')))
-for_hyprlock = 'hyprlock.png'
+images = os.listdir(path.replace(current, ""))
+for_hyprlock = "hyprlock.png"
 
 if for_hyprlock in images:
     images.pop(images.index(for_hyprlock))
@@ -22,9 +22,9 @@ else:
 
 next = path.replace(current, next)
 
-swww_command = f'swww img {next} --transition-step 90 --transition-angle 45 --transition-type outer --transition-fps 60 --transition-pos {float(randrange(1, 9, 1))/10},{float(randrange(0, 10, 1))/10}'
+swww_command = f"swww img {next} --transition-step 90 --transition-angle 45 --transition-type outer --transition-fps 60 --transition-pos {float(randrange(1, 9, 1)) / 10},{float(randrange(0, 10, 1)) / 10}"
 
-cp_command = f'cp {next} {path.replace(current, for_hyprlock)}'
+ln_command = f"ln -s -f {next} {path.replace(current, for_hyprlock)}"
 
 os.system(swww_command)
-os.system(cp_command)
+os.system(ln_command)
